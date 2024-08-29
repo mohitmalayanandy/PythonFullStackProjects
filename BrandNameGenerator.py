0c3b9bf136d316eedfac9c5b67e85a995f29a174
mohitmalayanandy <mohit78nandy@gmail.com>
2024-08-30 02:08:53 +0530

    Brand Name Generator has created

---
+++ BrandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator")
+city = input("Enter the name of your city: ")
+pet = input("Enter the name of your pet: ")
+print("Your brand name could be " + "\"" + city + " " + pet + "\"")
